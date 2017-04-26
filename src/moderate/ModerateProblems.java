c18946595a38d918e78b7826905dda308d5d7ec3
tripti-bishnoi <tripti.bishnoi29@gmail.com>
2017-04-26 00:51:21 -0700

    Problem 16.1 and 16.2

--- src/moderate/ModerateProblems.java
+++ src/moderate/ModerateProblems.java
@@ -3,6 +3,8 @@
  */
 package moderate;
 
+import java.util.HashMap;
+
 /**
  * @author TriptiAshishUpadhyay
  *
@@ -12,15 +14,37 @@ public class ModerateProblems {
 	//Problem 16.1
 	public void numberSwapper(int var1, int var2){
 		System.out.println("before: var1- "+ var1 + ", var2- "+var2);
-		var1 = var1 + var2;
-		var2 = var1 - var2;
-		var1 = var1 - var2;
+		var1 = var1 ^ var2;
+		var2 = var1 ^ var2;
+		var1 = var1 ^ var2;
 		System.out.println("after: var1- "+ var1 + ", var2- "+var2);
 	}
-	
+
+	//Problem 16.2
+	HashMap<String, Integer> bookHash = new HashMap<String, Integer>();
+
+	public int wordFreq(String[] book, String word){
+		word = word.trim().toLowerCase();
+		int count = 0;
+		if(bookHash.get(word) != null) return bookHash.get(word);
+		else{
+			for(String w: book){
+				if(w.trim().toLowerCase().equals(word)){
+					count++;
+				}
+			}
+			bookHash.put(word, count);
+		}
+		return count;
+	}
+
 	public static void main(String[] args) {
 		ModerateProblems obj = new ModerateProblems();
 		obj.numberSwapper(4, 10);
+		String[] book = {"my ", " name", "my", "my  ", "name    "};
+		String word = "MY";
+		System.out.println(obj.wordFreq(book,word));
+		System.out.println(obj.wordFreq(book,word));
 	}
 
 }
